078174723c966a853488c9289df3b4342a080949
divinnzirorera <118374661+divinnzirorera@users.noreply.github.com>
2023-05-08 20:44:18 +0200

    Update views.php

--- views.php
+++ views.php
@@ -4,13 +4,12 @@ $sql = "SELECT * FROM User";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    echo "<table ><tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Email</th><th>Password</th><th>Gender</th><th>Action</th></tr>";
+    echo "<table ><tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Email</th><th>Gender</th><th>update</th><th>delete</th></tr>";
     while($row = $result->fetch_assoc()) {
-        echo "<tr><td>" . $row["id"]. "</td><td>" . $row["fname"]. "</td><td>" . $row["lname"]. "</td><td>" . $row["email"]. "</td><td>" . $row["
-        password"]. "</td><td>" . $row["gender"]. "</td><td><a href='update.php?id=".$row["id"]."'>Edit</a> | <form method='post' action='delete.php'><input type='hidden' name='id' value='".$row["id"]."'><button type='submit' name='delete'>Delete</button></form></td></tr>";
+        echo "<tr><td>" . $row["id"]. "</td><td>" . $row["fname"]. "</td><td>" . $row["lname"]. "</td><td>" . $row["email"]. "</td><td>"  . $row["GENDER"]. "</td><td><a href='update.php?id=".$row["id"]."'>Edit</a></td> <td> <form method='post' action='delete.php'><input type='hidden' name='id' value='".$row["id"]."'><button type='submit' name='delete'>Delete</button></form></td></tr>";
     }
     echo "</table>";
-    } else {crudPHPMysql
+    } else {
     echo "0 results";
     }
     
@@ -19,6 +18,5 @@ if ($result->num_rows > 0) {
     
     <html>
         <a href="signup.html" class="btn btn-info"> Create new record</a>
-        <a href="views.php" class="btn btn-info" > View records from database</a>
     </html>
     
